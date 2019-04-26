96de3cf209c7563ff448670505f65f17fde93d20
unknown <183107@lab-ocs.local>
2019-04-26 12:09:05 +0900

    test2

---
+++ routes/reference.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require("sqlite3").verbose();
+const async = require('async');
+const fs = require('fs');
+
+// 画像のurlを返す
+router.get('/',(req, res)=>{
+
+})
+
+module.exports = router;
---
+++ routes/update.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require("sqlite3").verbose();
+const async = require('async');
+const fs = require('fs');
+
+// DBを更新
+router.get('/',(req, res)=>{
+
+})
+module.exports = router;
